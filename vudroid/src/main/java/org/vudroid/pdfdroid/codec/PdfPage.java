e07ab024bdc16b4f574f587d7c78fbc5ef1010c5
archko <archko@sina.com>
2025-01-06 11:46:01 +0800

    add link,text

--- vudroid/src/main/java/org/vudroid/pdfdroid/codec/PdfPage.java
+++ vudroid/src/main/java/org/vudroid/pdfdroid/codec/PdfPage.java
@@ -13,6 +13,8 @@ import org.vudroid.core.entity.ReflowBean;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.legere.pdfiumandroid.PdfTextPage;
+
 public class PdfPage implements CodecPage {
 
     private long pageHandle = -1;
@@ -107,36 +109,37 @@ public class PdfPage implements CodecPage {
 
     public List<Hyperlink> getPageLinks() {
         List<Hyperlink> hyperlinks = new ArrayList<>();
-        /*Link[] links = page.getLinks();
-        if (null != links) {
-            for (Link link : links) {
+        List<io.legere.pdfiumandroid.PdfDocument.Link> links = page.getPageLinks();
+        if (!links.isEmpty()) {
+            for (io.legere.pdfiumandroid.PdfDocument.Link link : links) {
                 Hyperlink hyper = new Hyperlink();
-                Location loc = doc.resolveLink(link);
-                int page = doc.pageNumberFromLocation(loc);
-                hyper.setPage(page);
-                hyper.setBbox(new Rect((int) link.getBounds().x0, (int) link.getBounds().y0, (int) link.getBounds().x1, (int) link.getBounds().y1));
+                hyper.setPage((int) pageHandle);
+                hyper.setBbox(new Rect((int) link.getBounds().left, (int) link.getBounds().top, (int) link.getBounds().right, (int) link.getBounds().bottom));
 
-                if (page >= 0) {
+                if (link.getDestPageIdx() >= 0) {
                     hyper.setLinkType(Hyperlink.LINKTYPE_PAGE);
                 } else {
-                    hyper.setUrl(link.getURI());
+                    hyper.setUrl(link.getUri());
                     hyper.setLinkType(Hyperlink.LINKTYPE_URL);
                 }
                 hyperlinks.add(hyper);
             }
-        }*/
+        }
 
         return hyperlinks;
     }
 
     public List<ReflowBean> getReflowBean() {
-        /*byte[] result = ((Page) page).textAsText("preserve-whitespace,inhibit-spaces");
-        if (null != result) {
-            List<ReflowBean> reflowBeans = ParseTextMain.INSTANCE.parseAsTextList(result, (int) pageHandle);
-            return reflowBeans;
-        }*/
+        List<ReflowBean> reflowBeans = new ArrayList<>();
+        PdfTextPage textPage = page.openTextPage();
+        int charCount = textPage.textPageCountChars();
+        if (charCount > 0) {
+            String text = textPage.textPageGetText(0, charCount);
+            ReflowBean reflowBean = new ReflowBean(text, ReflowBean.TYPE_STRING, String.valueOf(pageHandle));
+            reflowBeans.add(reflowBean);
 
-        return null;
+        }
+        return reflowBeans;
     }
 
     public synchronized void recycle() {
@@ -158,38 +161,5 @@ public class PdfPage implements CodecPage {
         recycle();
         super.finalize();
     }
-
-    public Bitmap renderBitmap(int width, int height, RectF pageSliceBounds) {
-        Matrix matrix = new Matrix();
-        //matrix.postScale(width/getMediaBox().width(), -height/getMediaBox().height());
-        matrix.postTranslate(0, height);
-        matrix.postTranslate(-pageSliceBounds.left * width, -pageSliceBounds.top * height);
-        matrix.postScale(1 / pageSliceBounds.width(), 1 / pageSliceBounds.height());
-        return render(new Rect(0, 0, width, height), matrix);
-    }
-
-    public Bitmap render(Rect viewbox, Matrix matrix) {
-        int[] mRect = new int[4];
-        mRect[0] = viewbox.left;
-        mRect[1] = viewbox.top;
-        mRect[2] = viewbox.right;
-        mRect[3] = viewbox.bottom;
-
-        float[] matrixSource = new float[9];
-        float[] matrixArray = new float[6];
-        matrix.getValues(matrixSource);
-        matrixArray[0] = matrixSource[0];
-        matrixArray[1] = matrixSource[3];
-        matrixArray[2] = matrixSource[1];
-        matrixArray[3] = matrixSource[4];
-        matrixArray[4] = matrixSource[2];
-        matrixArray[5] = matrixSource[5];
-
-        int width = viewbox.width();
-        int height = viewbox.height();
-        int[] bufferarray = new int[width * height];
-        //nativeCreateView(docHandle, pageHandle, mRect, matrixArray, bufferarray);
-        return Bitmap.createBitmap(bufferarray, width, height, Bitmap.Config.RGB_565);
-    }
-
 }
+
