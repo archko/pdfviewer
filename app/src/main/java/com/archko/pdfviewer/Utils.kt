705787d724c18ff7cfba733ba44a7180749617ed
archko <archko@sina.com>
2025-04-21 08:46:28 +0800

    upgrade gradle

--- app/src/main/java/com/archko/pdfviewer/Utils.kt
+++ app/src/main/java/com/archko/pdfviewer/Utils.kt
@@ -10,4 +10,18 @@ object Utils {
         val scale: Float = context.getResources().getDisplayMetrics().density
         return (dip * scale + 0.5f).toInt()
     }
+
+    /**
+     * 获取屏幕高度(px)
+     */
+    fun getScreenHeight(context: Context): Int {
+        return context.resources.displayMetrics.heightPixels
+    }
+
+    /**
+     * 获取屏幕宽度(px)
+     */
+    fun getScreenWidth(context: Context): Int {
+        return context.resources.displayMetrics.widthPixels
+    }
 }
